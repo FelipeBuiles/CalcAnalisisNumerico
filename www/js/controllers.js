52d1185a87e8f9e4a97c4f72a939084ea4e06265
Juan Felipe Builes <jbuiles4@eafit.edu.co>
2014-03-09 16:57:47 -0500

    primera iteración búsqueda incremental

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,9 +18,42 @@ function HomeCtrl($scope,navSvc,$rootScope) {
 }
 
 function IncrementalSearchCtrl($scope) {
-    $scope.test = "incremental search";
+    $scope.calculate = function() {
+        var x0      = parseInt($scope.x0);
+        var delta   = parseInt($scope.delta);
+        var nIter   = parseInt($scope.nIter);
+
+        var fx0 = f(x0);
+
+        if(fx0 === 0) {
+            $scope.root = fx0;
+        } else {
+            var x1 = f(x0) + delta;
+            var counter = 1;
+            var fx1 = f(x1);
+            while((fx0*fx1 > 0) && (counter <= nIter)) {
+                x0 = x1;
+                fx0 = fx1;
+                x1 = x0 + delta;
+                fx1 = f(x1);
+                counter++;
+            }
+            if(fx1 === 0){
+                $scope.root = fx1;
+            } else {
+                if(fx0*fx1 < 0) {
+                    $scope.root = "between " + x0 + " and " + x1;
+                } else {
+                    $scope.root = "failure after " + nIter + " iterations";
+                }
+            }
+        }
+    }
 }
 
 function BisectionCtrl($scope) {
-    $scope.test = "bisection";
+}
+
+function f(x){
+    return 0;
 }
