51674669ba239ce57e68a915f5310fb03891b998
FelipeBuiles <jbuiles4@eafit.edu.co>
2014-03-08 21:31:18 -0500

    estructura de carpetas y primer intento con angular y topcoat

---
+++ platforms/android/CordovaLib/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /home/pipe/Documents/Org/CalcAnalisisNumerico/platforms/android/CordovaLib/bin/AndroidManifest.xml \
---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/home/pipe/Documents/Org/CalcAnalisisNumerico/platforms/android/gen/com/phonegap/helloworld/R.java \
+ : /home/pipe/Documents/Org/CalcAnalisisNumerico/platforms/android/res/drawable-xhdpi/icon.png \
+/home/pipe/Documents/Org/CalcAnalisisNumerico/platforms/android/res/drawable-hdpi/icon.png \
+/home/pipe/Documents/Org/CalcAnalisisNumerico/platforms/android/res/xml/config.xml \
+/home/pipe/Documents/Org/CalcAnalisisNumerico/platforms/android/res/drawable/icon.png \
+/home/pipe/Documents/Org/CalcAnalisisNumerico/platforms/android/res/drawable-ldpi/icon.png \
+/home/pipe/Documents/Org/CalcAnalisisNumerico/platforms/android/res/values/strings.xml \
+/home/pipe/Documents/Org/CalcAnalisisNumerico/platforms/android/res/drawable-mdpi/icon.png \
+/home/pipe/Documents/Org/CalcAnalisisNumerico/platforms/android/bin/AndroidManifest.xml \
