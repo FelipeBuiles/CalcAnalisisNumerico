14b329821199bea8cbeac503d6a0ba75a5abf96f
Juan Felipe Builes <jbuiles4@eafit.edu.co>
2014-03-09 15:28:35 -0500

    borré platforms/ para que deje de joder la vida

--- platforms/android/assets/www/js/directives.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* Directives */
-angular.module('calcnumerico.directives', [])
-    .directive('appVersion', ['version', function(version) {
-        return function(scope, elm, attrs) {
-          elm.text(version);
-}}])
--- platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-C:\Users\Felipe\Documents\GitHub\CalcAnalisisNumerico\platforms\android\gen\co\edu\eafit\calcnumerico\R.java \
- : C:\Users\Felipe\Documents\GitHub\CalcAnalisisNumerico\platforms\android\res\drawable\icon.png \
-C:\Users\Felipe\Documents\GitHub\CalcAnalisisNumerico\platforms\android\res\drawable-hdpi\icon.png \
-C:\Users\Felipe\Documents\GitHub\CalcAnalisisNumerico\platforms\android\res\drawable-ldpi\icon.png \
-C:\Users\Felipe\Documents\GitHub\CalcAnalisisNumerico\platforms\android\res\drawable-mdpi\icon.png \
-C:\Users\Felipe\Documents\GitHub\CalcAnalisisNumerico\platforms\android\res\drawable-xhdpi\icon.png \
-C:\Users\Felipe\Documents\GitHub\CalcAnalisisNumerico\platforms\android\res\values\strings.xml \
-C:\Users\Felipe\Documents\GitHub\CalcAnalisisNumerico\platforms\android\res\xml\config.xml \
-C:\Users\Felipe\Documents\GitHub\CalcAnalisisNumerico\platforms\android\bin\AndroidManifest.xml \
