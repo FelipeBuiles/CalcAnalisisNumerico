b014d37318c064065a54750fce41e39351054344
Juan Felipe Builes <jbuiles4@eafit.edu.co>
2014-03-24 20:07:02 -0500

    Ahora con 100% más gráficas.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,6 +17,45 @@ function HomeCtrl($scope,navSvc,$rootScope) {
     };
 }
 
+function graph(canvas, f) {
+    if (null==canvas || !canvas.getContext) return;
+    var axes={}, ctx=canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    axes.x0 = .5 + .5*canvas.width;  // x0 pixels from left to x=0
+    axes.y0 = .5 + .5*canvas.height; // y0 pixels from top to y=0
+    axes.scale = 40;                 // 40 pixels from x=0 to x=1
+    axes.doNegativeX = true;
+    showAxes(ctx,axes);
+    funGraph(ctx,axes,f,"rgb(11,153,11)",1); 
+}
+
+function showAxes(ctx, axes) {
+    var x0=axes.x0, w=ctx.canvas.width;
+    var y0=axes.y0, h=ctx.canvas.height;
+    var xmin = axes.doNegativeX ? 0 : x0;
+    ctx.beginPath();
+    ctx.strokeStyle = "rgb(128,128,128)"; 
+    ctx.moveTo(xmin,y0); ctx.lineTo(w,y0);  // X axis
+    ctx.moveTo(x0,0);    ctx.lineTo(x0,h);  // Y axis
+    ctx.stroke();
+}
+
+function funGraph (ctx,axes,func,color,thick) {
+    var xx, yy, dx=4, x0=axes.x0, y0=axes.y0, scale=axes.scale;
+    var iMax = Math.round((ctx.canvas.width-x0)/dx);
+    var iMin = axes.doNegativeX ? Math.round(-x0/dx) : 0;
+    ctx.beginPath();
+    ctx.lineWidth = thick;
+    ctx.strokeStyle = color;
+
+    for (var i=iMin;i<=iMax;i++) {
+        xx = dx*i; yy = scale*func.evaluate({x : xx/scale});
+        if (i==iMin) ctx.moveTo(x0+xx,y0-yy);
+        else         ctx.lineTo(x0+xx,y0-yy);
+    }
+    ctx.stroke();
+}
+
 function IncrementalSearchCtrl($scope) {
     $scope.calculate = function() {
         var f       = Parser.parse($scope.equation);    
@@ -53,11 +92,14 @@ function IncrementalSearchCtrl($scope) {
 
 function BisectionCtrl($scope) {
     $scope.calculate = function() {
-        var xi    = parseFloat($scope.xi);
-        var xs    = parseFloat($scope.xs);
-        var tol   = parseFloat($scope.tol);
-        var nIter = parseInt($scope.nIter);
-        var f     = Parser.parse($scope.equation);
+        var xi     = parseFloat($scope.xi);
+        var xs     = parseFloat($scope.xs);
+        var tol    = parseFloat($scope.tol);
+        var nIter  = parseInt($scope.nIter);
+        var f      = Parser.parse($scope.equation);
+        var canvas = document.getElementById("canvas");
+        if (null==canvas || !canvas.getContext) return;
+        graph(canvas, f);
 
         var fxi = f.evaluate({x : xi});
         var fxs = f.evaluate({x : xs});
